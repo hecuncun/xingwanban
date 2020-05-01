2435916604969834b69669f84bde8a81b8a2cd48
hecuncun <13753638431@163.com>
2020-05-02 00:25:06 +0800

    启动 引导页修改  我的页面实现

--- app/src/main/java/com/cvnchina/xingwanban/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/cvnchina/xingwanban/ui/fragment/MineFragment.kt
@@ -3,6 +3,7 @@ package com.cvnchina.xingwanban.ui.fragment
 import android.view.View
 import com.cvnchina.xingwanban.R
 import com.lhzw.bluetooth.base.BaseFragment
+import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Created by heCunCun on 2020/4/29
@@ -16,6 +17,9 @@ class MineFragment :BaseFragment(){
     }
 
     override fun initView(view: View) {
+        toolbar_title.text="个人中心"
+        toolbar_right_img.setImageResource(R.mipmap.icon_more)
+        toolbar_right_img.visibility=View.VISIBLE
     }
 
     override fun initListener() {
